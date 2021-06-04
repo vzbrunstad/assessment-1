d6e7fc66c2693d3932ff40161c2e012bf499277b
vzbrunstad <vzbrunstad@msn.com>
2021-06-04 15:14:14 -0500

    Vince B Assesment 1 Complete

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,75 @@
-# Write your solution here!
+def optimal_change(owed,paid): # Define the change function that returns a string with the correct change
+    
+
+    us_currency = {# Declare a directory of us currency.
+
+    '$100 bill': 100,
+    '$50 bill' : 50,
+    '$20 bill': 20,
+    '$10 bill': 10,
+    '$5 bill': 5,
+    '$1 bill': 1,
+    'quarter': .25,
+    'dime': .10,
+    'nickle': .05,
+    'penny': .01,
+
+    } 
+
+    
+    optimal_change_dir = {# Declare an optimal_change_dir to add the number of each bill/coin
+
+    '$100 bill': 0,
+    '$50 bill' : 0,
+    '$20 bill': 0,
+    '$10 bill': 0,
+    '$5 bill': 0,
+    '$1 bill': 0,
+    'quarter': 0,
+    'dime': 0,
+    'nickle': 0,
+    'penny': 0,
+
+    } 
+    
+
+    change = paid- owed # Calculate Change 
+    if change < 0:  # Test if enough money way paid
+        return  f"Insufficient funds, please pay an additional ${round(change * -1, 3)}."
+    elif change == 0: # Test for exact change
+        return "Exact change paid, have a nice day." 
+    
+    
+    for x in us_currency: # step through each key in the us_currency directory
+        
+        while (change>=us_currency[x]): # adds current bill/coin to optimal_change_dir and removes from change   
+            optimal_change_dir[x] +=+1 
+            change -= us_currency [x]
+            change =  round(change,4)
+    
+    
+    for z in optimal_change_dir: # checks the position of last non-zero value in optimal_change_dir
+        if optimal_change_dir[z]!=0:
+            optimal_change_last_value = z
+
+
+    optimal_change_str = f"The optimal change for an item that costs ${owed} with an amount paid of ${paid} is"
+    i = 0
+    for y in optimal_change_dir: # Add # of each bill in optimal_change_dir to the optimal_change_string
+        
+        if y == 'penny' and optimal_change_dir[y]!=0: # check for the pennies condition
+            optimal_change_str += f"{', and'if y==optimal_change_last_value and i!=0 else ''}"
+            optimal_change_str += f" {optimal_change_dir[y]} "
+            optimal_change_str += f"{y if optimal_change_dir[y] == 1 else 'pennies'}"
+
+        elif optimal_change_dir[y]!=0: # handle all other non zero bills/coins
+            optimal_change_str += f"{''if i == 0 else ','}"
+            optimal_change_str += f"{' and'if y==optimal_change_last_value and i!=0 else ''}"
+            optimal_change_str += f" {optimal_change_dir[y]} {y}"
+            optimal_change_str += f"{''if optimal_change_dir[y] == 1 else 's'}" 
+            i+=1
+
+    optimal_change_str+='.' # Add a period to the end of each output
+
+    return optimal_change_str #returns the correct change as a string     
+
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,38 @@
-# Write your unit tests here!
+import unittest
+from optimal_change import optimal_change
+
+class armstrong_test(unittest.TestCase):
+    
+    '''Tests the correct output if insufficient funds are paid'''
+    def test_returns_insufficient_funds(self):
+        self.assertEqual(optimal_change(62.13, 60), "Insufficient funds, please pay an additional $2.13.")
+    
+    '''Tests the correct output if exaxct change is paid'''
+    def test_returns_exact_change(self):
+        self.assertEqual(optimal_change(20, 20), "Exact change paid, have a nice day.")
+
+    '''Tests the correct output if only change is one denomination'''
+    def test_returns_one_denomination(self):
+        self.assertEqual(optimal_change(20, 40), "The optimal change for an item that costs $20 with an amount paid of $40 is 1 $20 bill.")
+
+    '''Tests when there is only 1 penny change'''
+    def test_returns_one_penny(self):
+        self.assertEqual(optimal_change(20, 20.01), "The optimal change for an item that costs $20 with an amount paid of $20.01 is 1 penny.")
+
+    '''Tests when there are two pennies change'''
+    def test_returns_one_penny(self):
+        self.assertEqual(optimal_change(20, 20.02), "The optimal change for an item that costs $20 with an amount paid of $20.02 is 2 pennies.")
+
+    '''Tests when there are no pennies with larger change'''
+    def test_returns_3(self):
+        self.assertEqual(optimal_change(20,1500), "The optimal change for an item that costs $20 with an amount paid of $1500 is 14 $100 bills, 1 $50 bill, 1 $20 bill, and 1 $10 bill.")
+
+    '''Test that the output string matches the expected string output with the correct number of bills, punctuation and plurals'''
+
+    def test_returns_1(self):
+        self.assertEqual(optimal_change(62.13, 100), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+    def test_returns_2(self):
+        self.assertEqual(optimal_change(31.51, 50), "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+
+if __name__ == '__main__':
+    unittest.main()
